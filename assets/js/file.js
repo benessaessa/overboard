4b94df45f29087dc6ed549cca59f4b45d56dacbd
AzureAD\MohamedEissa <mohamed.eissa@grinta.ai>
2023-09-26 10:51:23 +0300

    email automations

--- assets/js/file.js
+++ assets/js/file.js
@@ -1,3 +1,12 @@
+// $(document).ready(function() {
+//     $('.dropdown').hover(function() {
+//       $(this).addClass('show');
+//       $(this).find('.dropdown-menu').addClass('show');
+//     }, function() {
+//       $(this).removeClass('show');
+//       $(this).find('.dropdown-menu').removeClass('show');
+//     });
+// });
 document.addEventListener("DOMContentLoaded", function(){
     /////// Prevent closing from click inside dropdown
     document.querySelectorAll('.dropdown-menu').forEach(function(element){
